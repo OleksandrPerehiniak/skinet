f04a5794dbe7b76a96e3eaffabdfcf70e0719191
DESKTOP-UPF14UF\Sasha <oleksandr.perehiniak.ki.2021@lpnu.ua>
2025-09-21 20:36:32 +0300

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,23 +6,19 @@ using Infrastructure.Data;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using API.RquestHelpers;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-
-public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
+public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
 {
 
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery]ProductSpecParams specParams)
     {
-        var spec = new ProductSpecification(brand, type, sort);
+        var spec = new ProductSpecification(specParams);
 
-        var products = await repo.ListAsync(spec);
-
-        return Ok(products);
+        return await CreatePageResult(repo, spec, specParams.PageIndex, specParams.PageSize);
     }
 
 
