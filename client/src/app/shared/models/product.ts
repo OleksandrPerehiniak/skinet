cfbe3ad096c3a712961a2731771b47a5d27c6bcd
DESKTOP-UPF14UF\Sasha <oleksandr.perehiniak.ki.2021@lpnu.ua>
2025-10-06 01:09:23 +0300

    End of section 8

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    pectureUrl: string;
+    price: number;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+}
