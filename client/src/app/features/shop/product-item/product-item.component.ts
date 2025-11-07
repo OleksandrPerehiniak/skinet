362f302ce66d8faf80ab7a811ecc15102fec16ee
DESKTOP-UPF14UF\Sasha <oleksandr.perehiniak.ki.2021@lpnu.ua>
2025-11-08 01:50:43 +0200

    added routing

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -6,10 +6,11 @@ import { CurrencyPipe } from '@angular/common';
 import { MatCardActions } from '@angular/material/card';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
-  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatButton, MatIcon],
+  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatButton, MatIcon, RouterLink],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
